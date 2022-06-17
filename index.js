a03a45d704bf9989cdf1fa5eae5842aadc6b01e1
Patrick <patrickcannady@gmail.com>
2022-06-17 13:57:57 -0500

    commit tree 65285c067cc61f64157004d81e278409692fc92b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const allCats = [...cats]
+    allCats.push(name);
+    return allCats
+}
+function prependCat(name) {
+    const allCats = [...cats]
+    allCats.unshift(name);
+    return allCats
+}
+function removeLastCat(name) {
+    const allCats = [...cats]
+    allCats.pop()
+    return allCats
+}
+function removeFirstCat(name) {
+    const allCats = [...cats]
+    allCats.shift()
+    return allCats;
+}
